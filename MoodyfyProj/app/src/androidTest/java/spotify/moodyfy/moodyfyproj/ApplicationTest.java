cb13e561da521477c56853584dc8ca4fb7a1c4a7
Shreyansh Kumar <shreyansh.kumar@hotmail.com>
2015-05-30 21:07:45 -0400

    added starter package

---
+++ MoodyfyProj/app/src/androidTest/java/spotify/moodyfy/moodyfyproj/ApplicationTest.java
@@ -0,0 +1,13 @@
+package spotify.moodyfy.moodyfyproj;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
